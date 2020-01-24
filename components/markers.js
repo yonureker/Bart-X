0643e71959e9f54f4b3e7bf39beb6f1dd08164a4
yonureker <yonureker@gmail.com>
2020-01-24 13:44:36 -0800

    add libraries

--- components/markers.js
+++ components/markers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import { useSelector } from 'react-redux'
