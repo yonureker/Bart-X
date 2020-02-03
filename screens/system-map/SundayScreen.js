6aa518e475a2685feb223b8a9e39fd3cc9928091
yonureker <yonureker@gmail.com>
2020-02-03 11:50:26 -0800

    remove pinch zoom

--- screens/system-map/SundayScreen.js
+++ screens/system-map/SundayScreen.js
@@ -1,42 +1,16 @@
 import React from "react";
+import { Image } from "react-native";
 import { Dimensions, Animated } from "react-native";
 import { PinchGestureHandler, State } from "react-native-gesture-handler";
 
 const SundayScreen = props => {
-  const { width } = Dimensions.get("window");
-  const scale = new Animated.Value(1);
-
-  const onZoomEvent = Animated.event(
-    [
-      {
-        nativeEvent: { scale: scale }
-      }
-    ],
-    {
-      useNativeDriver: true
-    }
-  );
-
-  const onZoomStateChange = event => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      Animated.spring(scale, {
-        toValue: 1,
-        useNativeDriver: true
-      }).start();
-    }
-  };
 
   return (
-      <PinchGestureHandler
-        onGestureEvent={onZoomEvent}
-        onHandlerStateChange={onZoomStateChange}
-      >
-        <Animated.Image
-          style={{ width: width, height: '100%', transform: [{ scale: scale }] }}
+        <Image
+          style={{ width: '100%', height: '100%'}}
           source={require("../../assets/sunday-system-map.png")}
           resizeMode="contain"
         />
-      </PinchGestureHandler>
   );
 };
 
--- screens/system-map/WeekAndSatScreen.js
+++ screens/system-map/WeekAndSatScreen.js
@@ -1,42 +1,14 @@
 import React from "react";
-import { Dimensions, Animated  } from "react-native";
-import { PinchGestureHandler, State } from "react-native-gesture-handler";
+import { Image } from "react-native";
 
 const WeekAndSatScreen = props => {
-  const { width } = Dimensions.get("window");
-  const scale = new Animated.Value(1);
-
-  const onZoomEvent = Animated.event(
-    [
-      {
-        nativeEvent: { scale: scale }
-      }
-    ],
-    {
-      useNativeDriver: true
-    }
-  );
-
-  const onZoomStateChange = event => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      Animated.spring(scale, {
-        toValue: 1,
-        useNativeDriver: true
-      }).start();
-    }
-  };
 
   return (
-      <PinchGestureHandler
-        onGestureEvent={onZoomEvent}
-        onHandlerStateChange={onZoomStateChange}
-      >
-        <Animated.Image
-          style={{ width: width, height: '100%', transform: [{ scale: scale }] }}
+        <Image
+          style={{ width: '100%', height: '100%' }}
           source={require("../../assets/week-and-sat-system-map.png")}
           resizeMode="contain"
         />
-      </PinchGestureHandler>
   );
 };
 
