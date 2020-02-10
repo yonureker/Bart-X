a56871a3178592eb240d9fee248a3075d716880c
yonureker <yonureker@gmail.com>
2020-02-09 22:30:28 -0800

    add list creen

---
+++ screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, FlatList } from 'react-native'
+
+const ListScreen = (props) => {
+  return(
+    <View>
+
+    </View>
+  )
+}
+
+export default ListScreen;
