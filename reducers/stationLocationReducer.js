6f412793e0c79ac87204c279123bc9a1c8f906c9
yonureker <yonureker@gmail.com>
2020-02-10 21:08:43 -0800

    update reducer

--- reducers/stationLocationReducer.js
+++ reducers/stationLocationReducer.js
@@ -3,7 +3,7 @@ const initialState = [];
 const stationLocationReducer = (state = initialState , action) => {
   switch(action.type) {
     case 'RECEIVE_STATION_LOCATIONS':
-      return action.payload
+      return action.payload;
     default:
       return state;
   }
