542b12c0bb58a982811f81a7f5c1cc7c57434f4a
yonureker <yonureker@gmail.com>
2020-02-12 17:26:00 -0800

    create screens

---
+++ screens/station-schedules/StationDetailsScreen.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import { ScrollView } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
+
+const StationDetailsScreen = props => {
+  const stationList = useSelector(state => state.trainDepartures);
+
+  const selectedStation = stationList.find(
+    item => item.name == props.navigation.state.params.station
+  );
+
+  const trainList = () =>
+    selectedStation.etd.map((route) => {
+      return route.estimate.map((train, index) => {
+        const color = train.color.toLowerCase();
+
+        return (
+          <View style={styles.train} key={index}>
+            <View style={{ ...styles.left, backgroundColor: color }}></View>
+            <View style={styles.mid}>
+              <View>
+                <Text style={{ fontSize: 20 }}>{route.destination}</Text>
+              </View>
+              <View>
+                <Text style={{ color: "#A2AEB1" }}>
+                  {train.length} cars | Platform {train.platform}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.right}>
+              <Text style={{ fontSize: 20 }}>{train.minutes}</Text>
+            </View>
+          </View>
+        );
+      });
+    });
+
+  return <ScrollView>{trainList()}</ScrollView>;
+};
+
+StationDetailsScreen.navigationOptions = ({navigation}) => ({
+  title: navigation.state.params.station,
+  headerLeft: () => (
+    <Ionicons
+      name="md-locate"
+      size={25}
+      color="black"
+      style={{marginLeft: 20}}
+      onPress={() => navigation.goBack()}
+    />
+  ),
+  headerRight: () => (
+    <Ionicons
+      name="md-refresh"
+      size={25}
+      color="black"
+      style={{marginRight: 20}}
+      onPress={() => navigation.state.params.fetchTrainDepartures()}
+    />
+  )
+});
+
+const styles = StyleSheet.create({
+  train: {
+    height: 60,
+    justifyContent: "center",
+    flexDirection: "row",
+    fontSize: 20,
+    borderColor: "#F0F4F5",
+    borderBottomWidth: 1,
+    paddingLeft: 1,
+    paddingRight: 10
+  },
+  left: {
+    width: "5%",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 50
+  },
+  mid: {
+    width: "70%",
+    justifyContent: "space-evenly",
+    flexDirection: "column",
+    paddingLeft: 10
+  },
+  right: {
+    width: "20%",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+export default StationDetailsScreen;
---
+++ screens/station-schedules/StationListScreen.js
@@ -0,0 +1,151 @@
+import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  ActivityIndicator
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import { getDistance, convertDistance } from "geolib";
+
+import StationList from "../../components/stationList";
+
+const StationListScreen = props => {
+  const dispatch = useDispatch();
+  const userLocation = useSelector(state => state.userLocation);
+  const stations = useSelector(state => state.stationLocations);
+  const departures = useSelector(state => state.trainDepartures)
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  useEffect(() => {
+    fetchTrainDepartures();
+  }, []);
+
+  useEffect(() => {
+    fetchStationLocation();
+  }, []);
+
+  // useEffect(() => {
+  //   calculateDistance();
+  // }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(fetchTrainDepartures, 5000);
+    return () => clearInterval(intervalId);
+    
+  });
+
+  const fetchStationLocation = () => {
+    fetch(
+      "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y"
+    )
+      .then(response => response.json())
+      .then(responseJson =>
+        dispatch({
+          type: "RECEIVE_STATION_LOCATIONS",
+          payload: responseJson.root.stations.station
+        })
+      )
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const fetchTrainDepartures = () => {
+    // setStationList(responseJson.root.station);
+    // setLastUpdate(responseJson.root.time);)
+    // call BART API
+    fetch(
+      "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&key=MW9S-E7SL-26DU-VV8V&json=y"
+    )
+      .then(response => response.json())
+      .then(responseJson =>
+        dispatch({
+          type: "RECEIVE_TRAIN_DEPARTURE_DATA",
+          payload: responseJson.root.station
+        })
+      )
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      dispatch({
+        type: "RECEIVE_USER_LOCATION",
+        payload: { coords: { latitude: 37.792874, longitude: -122.39703 } }
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    dispatch({
+      type: "RECEIVE_USER_LOCATION",
+      payload: location
+    });
+  };
+
+  const calculateDistance = () => {
+    return stations.map(station => {
+      return {
+        ...station,
+        distance: convertDistance(
+          getDistance(
+            {
+              latitude: station.gtfs_latitude,
+              longitude: station.gtfs_longitude
+            },
+            {
+              latitude: String(userLocation.coords.latitude),
+              longitude: String(userLocation.coords.longitude)
+            }
+          ),
+          "mi"
+        )
+      };
+    });
+  };
+
+  const stationsWithDistance = calculateDistance();
+
+  if (userLocation && stations.length !== 0 && departures.length !== 0) {
+    return (
+      <View style={styles.container}>
+        <StatusBar />
+        <SafeAreaView style={{ width: "100%" }}>
+          <StationList
+            navigate={props.navigation.navigate}
+            stations={stationsWithDistance}
+            fetchTrainDepartures={() => fetchTrainDepartures}
+          />
+        </SafeAreaView>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Loading Stations</Text>
+        <ActivityIndicator size="large" color="blue"/>
+      </View>
+    );
+  }
+};
+
+StationListScreen.navigationOptions = {
+  title: "Closest Stations"
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" }
+});
+
+export default StationListScreen;
