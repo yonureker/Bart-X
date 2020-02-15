88311c9fa850acc4eaf434b339c8134d15644be0
yonureker <yonureker@gmail.com>
2020-02-14 17:08:02 -0800

    convert map provider to default

--- screens/LiveMapScreen.js
+++ screens/LiveMapScreen.js
@@ -19,20 +19,21 @@ const LiveMapScreen = React.memo((props) => {
     <StatusBar />
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
+        region={{
           latitude: parseFloat(latitude) || 37.792874,
           longitude: parseFloat(longitude) || -122.39703,
           latitudeDelta: 0.04,
           longitudeDelta: 0.04
         }}
-        minZoomLevel={10}
-        provider={"google"}
+        minZoomLevel={1}
+        provider={null}
         loadingEnabled={true}
         mapType={"standard"}
         showsPointsOfInterest={false}
         showsBuildings={false}
         showsTraffic={false}
         showsIndoors={false}
+        showsCompass={true}
       >
         <Markers />
       </MapView>
