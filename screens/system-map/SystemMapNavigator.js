5057218df182d94dffb88d90e769d2606f242072
yonureker <yonureker@gmail.com>
2020-02-26 13:05:45 -0800

    add system map navigator

---
+++ screens/system-map/SystemMapNavigator.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StatusBar, View, SafeAreaView } from "react-native";
+import {
+  createMaterialTopTabNavigator,
+  MaterialTopTabBar
+} from "react-navigation-tabs";
+
+import SundayScreen from "./SundayScreen";
+import WeekAndSatScreen from "./WeekAndSatScreen";
+
+const SystemMapNavigator = createMaterialTopTabNavigator(
+  {
+    "Weekday & Saturday": {
+      screen: WeekAndSatScreen,
+      navigationOptions: {
+        swipeEnabled: false
+      }
+    },
+    Sunday: {
+      screen: SundayScreen,
+      navigationOptions: {
+        swipeEnabled: false
+      }
+    }
+  },
+  {
+    tabBarComponent: SafeAreaMaterialTopTabBar
+  }
+);
+
+function SafeAreaMaterialTopTabBar(props) {
+  return (
+    <View>
+      <StatusBar />
+      <SafeAreaView
+        style={{
+          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+        }}
+      >
+        <MaterialTopTabBar {...props} />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+export default SystemMapNavigator;
