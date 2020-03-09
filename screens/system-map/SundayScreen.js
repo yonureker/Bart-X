8587da6e772c16a8aa430d1bf361ecfe63329cda
yonureker <yonureker@gmail.com>
2020-03-09 15:17:38 -0700

    clear comp

--- screens/system-map/SundayScreen.js
+++ screens/system-map/SundayScreen.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { Image } from "react-native";
-import PinchZoomView from 'react-native-pinch-zoom-view';
+import PinchZoomView from "react-native-pinch-zoom-view";
 
-const SundayScreen = props => {
-
+export default function SundayScreen() {
   return (
     <PinchZoomView minScale={1} maxScale={4}>
-        <Image
-          style={{ width: '100%', height: '100%'}}
-          source={require("../../assets/sunday-system-map.png")}
-          resizeMode="contain"
-        />
-        </PinchZoomView>
+      <Image
+        style={{ width: "100%", height: "100%" }}
+        source={require("../../assets/sunday-system-map.png")}
+        resizeMode="contain"
+      />
+    </PinchZoomView>
   );
-};
-
-export default SundayScreen;
+}
