fb2aa2b976ca550630240abf8fb246d0c94665b1
yonureker <yonureker@gmail.com>
2020-03-08 23:17:13 -0700

    add colortheme

--- components/stationList.js
+++ components/stationList.js
@@ -1,24 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, FlatList, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useColorScheme } from "react-native-appearance";
 
 const StationList = props => {
-  const stationList = props.stations.sort((a,b) => a.distance > b.distance ? 1 : -1);
+  const colorScheme = useColorScheme();
+  const stationList = props.stations.sort((a, b) =>
+    a.distance > b.distance ? 1 : -1
+  );
+
+  const titleText =
+    colorScheme === "dark" ? styles.darkTitle : styles.lightTitle;
+  const distanceText =
+    colorScheme === "dark" ? styles.darkDistance : styles.lightDistance;
 
   return (
     <FlatList
       data={stationList}
       renderItem={({ item }) => (
-        <TouchableOpacity style={styles.button} onPress={() => props.navigate('StationDetails', {
-          abbr: item.abbr,
-          name: item.name,
-        })}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            props.navigate("StationDetails", {
+              abbr: item.abbr,
+              name: item.name
+            })
+          }
+        >
           <View>
-            <Text style={styles.stationTitle}>{item.name}</Text>
+            <Text style={[styles.stationTitle, titleText]}>{item.name}</Text>
           </View>
           <View>
-            <Text style={styles.stationDistance}>
-              {(item.distance).toFixed(2)} miles
+            <Text style={[styles.stationDistance, distanceText]}>
+              {item.distance.toFixed(2)} miles
             </Text>
           </View>
         </TouchableOpacity>
@@ -31,7 +45,7 @@ const StationList = props => {
 const styles = StyleSheet.create({
   button: {
     height: 50,
-    borderColor: '#F0F4F5',
+    borderColor: "#F0F4F5",
     borderBottomWidth: 1,
     paddingLeft: 10,
     marginTop: 10,
@@ -40,9 +54,21 @@ const styles = StyleSheet.create({
   stationTitle: {
     fontSize: 20
   },
+  lightTitle: {
+    color: "black"
+  },
+  darkTitle: {
+    color: "white"
+  },
+  lightDistance: {
+    color: "black"
+  },
+  darkDistance: {
+    color: "white"
+  },
   stationDistance: {
     fontSize: 12
   }
-})
+});
 
 export default StationList;
