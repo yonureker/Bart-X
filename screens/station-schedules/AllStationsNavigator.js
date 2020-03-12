761da4fa68dc405bb15d39470562e9b655b9b3ac
yonureker <yonureker@gmail.com>
2020-03-11 21:23:09 -0700

    add favorite icon

--- screens/station-schedules/AllStationsNavigator.js
+++ screens/station-schedules/AllStationsNavigator.js
@@ -1,13 +1,14 @@
 import StationListScreen from "./StationListScreen";
 import StationDetailsScreen from "./StationDetailsScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { useColorScheme } from "react-native-appearance";
+import * as SecureStore from "expo-secure-store";
 
 const Stack = createStackNavigator();
 
-export default function AllStationsNavigator() {
+export default function AllStationsNavigator(props) {
   const scheme = useColorScheme();
 
   return (
@@ -29,6 +30,18 @@ export default function AllStationsNavigator() {
               color={scheme === "dark" ? "white" : "black"}
               style={{ marginLeft: 20 }}
               onPress={() => navigation.goBack()}
+            />
+          ),
+          headerRight: () => (
+            <MaterialIcons
+              name={
+                SecureStore.getItemAsync(route.params.abbr) !== "true"
+                  ? "favorite"
+                  : "favorite-border"
+              }
+              size={30}
+              color="red"
+              style={{ marginRight: 20 }}
             />
           )
         })}
