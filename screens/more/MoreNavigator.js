1dae0a284c9b1b9d0c60875e4136ad8386d9a418
yonureker <yonureker@gmail.com>
2020-04-07 23:23:26 -0700

    add new navigator

---
+++ screens/more/MoreNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import AboutScreen from '../AboutScreen'
+
+const Drawer = createDrawerNavigator();
+
+export default function MoreNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="AboutScreen" component={AboutScreen} />
+    </Drawer.Navigator>
+  );
+}
