6327ba6faede40961ea94a41c283ba42b51fb649
yonureker <yonureker@gmail.com>
2020-05-25 01:02:02 -0700

    update version

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -15,7 +15,7 @@ const AboutScreen = (props) => {
   return (
     <View style={styles.container}>
       <View>
-        <Text style={[styles.font, fontColor]}>Version 1.4.1</Text>
+        <Text style={[styles.font, fontColor]}>Version 1.5</Text>
       </View>
       <View style={styles.content}>
         <View>
