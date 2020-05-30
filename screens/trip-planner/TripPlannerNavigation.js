5914f1dbbc60ec7ed0038eb7508debe62e2ee815
yonureker <yonureker@gmail.com>
2020-05-30 09:15:14 -0700

    create navigation

---
+++ screens/trip-planner/TripPlannerNavigation.js
@@ -0,0 +1,25 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState, useEffect } from "react";
+import { useColorScheme } from "react-native-appearance";
+import TripPlannerHomeScreen from "./TripPlannerHomeScreen";
+
+const Stack = createStackNavigator();
+
+export default function TripPlannerNavigator(props) {
+  const scheme = useColorScheme();
+  const {
+    stations: { station }
+  } = require("../../stations");
+
+  return (
+    <Stack.Navigator
+      initialRouteName="TripPlanner Home"
+      screenOptions={{
+        gestureEnabled: false,
+        headerTitleStyle: { fontSize: 18 }
+      }}
+    >
+      <Stack.Screen name="TripPlanner Home" component={TripPlannerHomeScreen} />
+    </Stack.Navigator>
+  );
+}
