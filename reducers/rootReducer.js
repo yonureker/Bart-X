bcaef64ea6bdaa47afa3c99833ff5bb3435d4da1
yonureker <yonureker@gmail.com>
2020-05-29 23:32:01 -0700

    add new reducers

--- reducers/rootReducer.js
+++ reducers/rootReducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import userLocationReducer from "./userLocationReducer";
 import trainDepartureReducer from "./trainDepartureReducer";
+import searchTextReducer from './searchTextReducer';
+import searchBarReducer from "./searchBarReducer";
 
 const rootReducer = combineReducers({
   userLocation: userLocationReducer,
-  trainDepartures: trainDepartureReducer
+  trainDepartures: trainDepartureReducer,
+  searchText: searchTextReducer,
+  searchBar: searchBarReducer
 });
 
 export default rootReducer;
