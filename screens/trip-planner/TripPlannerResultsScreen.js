8bf942e55dfd868134c3bb4182974bcc846d6539
yonureker <yonureker@gmail.com>
2020-06-02 23:16:44 -0700

    add fetch and useEffect

--- screens/trip-planner/TripPlannerResultsScreen.js
+++ screens/trip-planner/TripPlannerResultsScreen.js
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text, Modal, Button, Picker } from "react-native";
 
 const TripPlannerResultsScreen = (props) => {
+  const {date, departure, destination, time, option} = props.route.params
+
+  useEffect(() => {
+    fetchTripResults()
+  }, [])
+
+  const fetchTripResults = () => {
+    fetch(
+      `http://api.bart.gov/api/sched.aspx?cmd=${option}&orig=${departure.abbr}&dest=${destination.abbr}&date=${date}&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=0&time=${time}&json=y`
+    )
+      .then((response) => response.json())
+      .then((responseJson) => console.log(responseJson));
+  };
+  
   return (
     <View>
       <Text>Hello</Text>
     </View>
   );
 
-  const fetchTripResults = () => {
-    fetch(
-      "http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=ASHB&dest=CIVC&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=0&time=3:33pm&json=y"
-    )
-      .then((response) => response.json())
-      .then((responseJson) => setResults);
-  };
 };
 
 export default TripPlannerResultsScreen;
