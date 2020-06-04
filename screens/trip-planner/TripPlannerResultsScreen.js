19389cb2bc442f53bffaf58fb5e430a99570dff6
yonureker <yonureker@gmail.com>
2020-06-03 22:11:47 -0700

    add local state

--- screens/trip-planner/TripPlannerResultsScreen.js
+++ screens/trip-planner/TripPlannerResultsScreen.js
@@ -1,27 +1,52 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, Modal, Button, Picker } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 const TripPlannerResultsScreen = (props) => {
-  const {date, departure, destination, time, option} = props.route.params
+  const { date, departure, destination, time, option } = props.route.params;
+
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    fetchTripResults()
-  }, [])
+    fetchTripResults();
+  }, []);
 
   const fetchTripResults = () => {
     fetch(
-      `http://api.bart.gov/api/sched.aspx?cmd=${option}&orig=${departure.abbr}&dest=${destination.abbr}&date=${date}&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=0&time=${time}&json=y`
+      `http://api.bart.gov/api/sched.aspx?cmd=${option}&orig=${departure.abbr}&dest=${destination.abbr}&date=${date}&key=MW9S-E7SL-26DU-VV8V&b=0&a=4&time=${time}&l=1&json=y`
     )
       .then((response) => response.json())
-      .then((responseJson) => console.log(responseJson));
+      .then((responseJson) => setData(responseJson.root.schedule.request));
   };
-  
-  return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  );
 
+  if (data.trip) {
+    return (
+      <View style={styles.container}>
+        {data.trip.map((elem, index) => (
+          <View style={styles.elemBox}>
+            <Text>Total Trip Time: {elem["@tripTime"]} min</Text>
+          </View>
+        ))}
+      </View>
+    );
+  }
+
+  return <View style={styles.container}></View>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  tripBox: {
+    width: "95%",
+    backgroundColor: "blue",
+    height: "20%",
+    borderWidth: 1,
+    borderColor: "#E6E8ED",
+    borderRadius: 15,
+  },
+});
 
 export default TripPlannerResultsScreen;
