f89c0d41640fda97d803ca1aea9800c50d2a6236
yonureker <yonureker@gmail.com>
2020-06-05 23:11:58 -0700

    big update

--- screens/trip-planner/TripPlannerResultsScreen.js
+++ screens/trip-planner/TripPlannerResultsScreen.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text, ScrollView } from "react-native";
 import { useColorScheme } from "react-native-appearance";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 const TripPlannerResultsScreen = (props) => {
   const { date, departure, destination, time, option } = props.route.params;
 
   const [data, setData] = useState({});
   const colorScheme = useColorScheme();
+
+  const {
+    stations: { station },
+  } = require("../../stations");
+
+  const {
+    routes: { route },
+  } = require("../../routes");
 
   useEffect(() => {
     fetchTripResults();
@@ -27,22 +36,90 @@ const TripPlannerResultsScreen = (props) => {
   if (data.trip) {
     return (
       <ScrollView>
-      <View style={styles.container}>
-        {/* departure here */}
-        
-        {/* transfers here */}
-        {/* destination here */}
-        {data.trip.map((elem, index) => (
-          <View style={styles.tripBox}>
-            {elem.leg.map((leg, index) => (
-              <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
-              <View><Text>{leg["@origin"]}</Text></View>
-              <View><Text>{leg["@origTimeMin"]}</Text></View>
+        <View style={styles.container}>
+          {data.trip.map((elem, index) => (
+            <View style={styles.tripBox}>
+               <View style={{flexDirection: 'row'}}>
+                    <Text>Trip</Text>
+                    </View>
+              {elem.leg.map((x, index) => (
+                <View>
+                  <View style={[styles.train, backgroundStyle, {borderBottomWidth: 0}]}>
+                    <View
+                      style={[styles.left, {backgroundColor: route.find(elem => elem.routeID === x["@line"])["hexcolor"]}]}
+                    ></View>
+                    <View style={styles.mid}>
+                      <View>
+                        <Text style={[styles.destinationText, textStyle]}>
+                          Board @{station.find(elem => elem.abbr === x["@origin"])["name"]}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={[styles.platformText, textStyle]}>
+                          {x["@trainHeadStation"]} Train
+                        </Text>
+                      </View>
+                    </View>
+                    <View style={styles.right}>
+                      <View>
+                        <Text style={[styles.minutesText, textStyle]}>
+                          {x["@origTimeMin"]}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+
+                  <View style={[styles.train, backgroundStyle]}>
+                    <View
+                      style={{ ...styles.left, backgroundColor: route.find(elem => elem.routeID === x["@line"])["hexcolor"] }}
+                    ></View>
+                    <View style={styles.mid}>
+                      <View>
+                        <Text style={[styles.destinationText, textStyle]}>
+                          Arrive @{station.find(elem => elem.abbr === x["@destination"])["name"]}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={[styles.platformText, textStyle]}>
+                          {x["@trainHeadStation"]} Train
+                        </Text>
+                      </View>
+                    </View>
+                    <View style={styles.right}>
+                      <View>
+                        <Text style={[styles.minutesText, textStyle]}>
+                          {x["@destTimeMin"]}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+
+                  {index !== elem.leg.length - 1 && (
+                    <View style={[styles.train, backgroundStyle]}>
+                      <View
+                        style={[styles.left, {borderWidth: 0}]}
+                      >
+                        <MaterialCommunityIcons name="transit-connection" color="black" size={40} style={{paddingRight: 40}} />
+                      </View>
+                      <View style={styles.mid}>
+                        <View>
+                          <Text style={[styles.destinationText, textStyle]}>
+                            Transfer
+                          </Text>
+                        </View>
+                      </View>
+                      <View style={styles.right}>
+                        <View>
+                          <Text style={[styles.minutesText, textStyle]}></Text>
+                        </View>
+                      </View>
+                    </View>
+                  )}
+                </View>
+              ))}
             </View>
-            ))}
-          </View>
-        ))}
-      </View>
+          ))}
+        </View>
       </ScrollView>
     );
   }
@@ -61,13 +138,60 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   tripBox: {
-    padding: 15,
+    padding: 5,
     marginBottom: 20,
-    width: "95%",
+    width: "100%",
     backgroundColor: "#E6E8ED",
     borderWidth: 1,
     borderColor: "#E6E8ED",
-    borderRadius: 15,
+    borderRadius: 1,
+  },
+  train: {
+    height: 60,
+    justifyContent: "center",
+    flexDirection: "row",
+    borderColor: "#F0F4F5",
+    borderBottomWidth: 1,
+    paddingLeft: 1,
+    paddingRight: 10,
+  },
+  left: {
+    width: "3%",
+    marginTop: 2,
+    marginBottom: 2,
+    borderColor: "black",
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  mid: {
+    width: "75%",
+    justifyContent: "space-evenly",
+    flexDirection: "column",
+    paddingLeft: 5,
+  },
+  right: {
+    width: "18%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  destinationText: {
+    fontSize: 16,
+  },
+  platformText: {
+    color: "#A2AEB1",
+  },
+  minutesText: {
+    fontSize: 13,
+  },
+  darkBackground: {
+    backgroundColor: "black",
+  },
+  lightBackground: {
+    backgroundColor: "white",
+  },
+  lightText: {
+    color: "white",
   },
 });
 
