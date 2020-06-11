cc9f23c60b925b18701328651a97d9244c3c18bc
yonureker <yonureker@gmail.com>
2020-06-10 23:44:38 -0700

    create file

---
+++ screens/trip-planner/TripPlannerNavigator.js
@@ -0,0 +1,36 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import { useColorScheme } from "react-native-appearance";
+
+import TripPlannerHomeScreen from "./TripPlannerHomeScreen";
+import TripPlannerResultsNavigator from './TripPlannerResultsNavigator';
+
+const Stack = createStackNavigator();
+
+export default function TripPlannerNavigator(props) {
+  const scheme = useColorScheme();
+  const {
+    stations: { station }
+  } = require("../../stations");
+
+  return (
+    <Stack.Navigator
+      initialRouteName="TripPlanner Home"
+      screenOptions={{
+        gestureEnabled: false,
+        headerTitleStyle: { fontSize: 18 }
+      }}
+    >
+      <Stack.Screen
+        name="TripPlanner Home"
+        component={TripPlannerHomeScreen}
+        options={{ title: "Trip Planner" }}
+      />
+      <Stack.Screen
+        name="TripPlanner Results Navigator"
+        component={TripPlannerResultsNavigator}
+        options={{ title: "Results" }}
+      />
+    </Stack.Navigator>
+  );
+}
