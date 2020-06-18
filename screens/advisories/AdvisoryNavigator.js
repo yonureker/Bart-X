db9e2818cdcd1dd2e78955dfb932ec74e8681523
yonureker <yonureker@gmail.com>
2020-06-17 22:29:13 -0700

    update style

--- screens/advisories/AdvisoryNavigator.js
+++ screens/advisories/AdvisoryNavigator.js
@@ -3,41 +3,37 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useColorScheme } from "react-native-appearance";
 
-import AdvisoryScreen from '../advisories/AdvisoryScreen';
+import AdvisoryScreen from "../advisories/AdvisoryScreen";
 
 const Stack = createStackNavigator();
 
-export default function AdvisoryNavigator(){
+export default function AdvisoryNavigator() {
   const scheme = useColorScheme();
 
-  return(
+  return (
     <Stack.Navigator
       initialRouteName="Advisories"
       screenOptions={{
         headerTitleStyle: {
-          fontSize: 18
-        }
+          fontSize: 18,
+        },
       }}
     >
-    <Stack.Screen
-     name="Advisories"
-     component={AdvisoryScreen}
-     options={({ route, navigation }) => ({
-      headerRight: () => (
-        <MaterialCommunityIcons
-          name="refresh"
-          size={32}
-          color={scheme === "dark" ? "white" : "black"}
-          style={{ marginRight: 10 }}
-          onPress={() =>
-            dispatch({
-              type: "SHOW_SEARCH_BAR"
-            })
-          }
-        />
-      ),
-    })}
-    />
+      <Stack.Screen
+        name="Advisories"
+        component={AdvisoryScreen}
+        // options={({ route, navigation }) => ({
+        //   headerRight: () => (
+        //     <MaterialCommunityIcons
+        //       name="refresh"
+        //       size={32}
+        //       color={scheme === "dark" ? "white" : "black"}
+        //       style={{ marginRight: 10 }}
+        //       onPress={() => console.log("hello")}
+        //     />
+        //   ),
+        // })}
+      />
     </Stack.Navigator>
-  )
+  );
 }
