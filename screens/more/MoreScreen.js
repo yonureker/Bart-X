ee7535ba5e1ab86f014c6d6c51b6a71b062288fd
yonureker <yonureker@gmail.com>
2020-06-18 23:35:50 -0700

    add nodemailer

--- screens/more/MoreScreen.js
+++ screens/more/MoreScreen.js
@@ -4,72 +4,54 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useColorScheme } from "react-native-appearance";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const MoreScreen = (props) => {
+const MoreScreen = props => {
   const scheme = useColorScheme();
   const fontColor = scheme === "dark" ? styles.darkFont : null;
-
-  const handlePress = () => {
-    Linking.openURL("https://www.linkedin.com/in/onureker/");
-  };
 
   const items = [
     {
       iconName: "star-box",
-      iconLibrary: "MaterialCommunityIcons",
       title: "Rate Bart X",
       link: () => {
         Linking.openURL(
           "https://apps.apple.com/us/app/bart-x/id1480753570?action=write-review"
         );
-      },
+      }
     },
-    {
-      iconName: "email-box",
-      iconLibrary: "MaterialCommunityIcons",
-      title: "Send Feedback",
-      link: () => {
-        Linking.openURL("mailto:yonureker@gmail.com");
-      },
-    },
+    // {
+    //   iconName: "email-box",
+    //   title: "Send Feedback",
+    //   link: () => props.navigation.navigate("Feedback")
+    // },
     {
       iconName: "map-legend",
-      iconLibrary: "MaterialCommunityIcons",
       title: "System Maps",
-      link: () => props.navigation.navigate("System Map"),
-    },
+      link: () => props.navigation.navigate("System Map")
+    }
   ];
 
   return (
     <View style={styles.container}>
-      {/* <View>
-        <Text style={[styles.font, fontColor]}>Version 1.5.2</Text>
-      </View> */}
-      {/* <View style={styles.content}>
-        <View>
-          <Text style={[styles.font, fontColor]}>Built with React Native</Text>
-        </View>
-        <View>
-          <MaterialCommunityIcons name="react" size={25} color="#61DBFB" />
-        </View>
-      </View>
       <View style={styles.author}>
-        <Text style={[styles.font, fontColor]}>
-          Created by{" "}
-          <Text
-            style={{ textDecorationLine: "underline" }}
-            onPress={handlePress}
-          >
-            Onur Eker
-          </Text>
-          .
-        </Text>
-      </View> */}
-      <View style={styles.container}>
+        <View><Text style={styles.font}>Developer: Onur Eker</Text></View>
+        <View><MaterialCommunityIcons
+                  name="linkedin-box"
+                  size={30}
+                  color="gray"
+                /></View>
+        <View><MaterialCommunityIcons
+                  name="github-box"
+                  size={30}
+                  color="gray"
+                /></View>
+      </View>
+      <View style={styles.box}>
         {items.map((item, index) => (
           <TouchableOpacity
             key={index}
             onPress={item.link}
-            style={{ alignItems: "center" }}
+            style={{borderBottomWidth:1, borderBottomColor: '#F0F4F5'}}
+            // style={{alignItems: 'center', justifyContent: 'center'}}
           >
             <View style={styles.item} key={index}>
               <View style={styles.itemLeft}>
@@ -102,39 +84,52 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    width: '100%',
   },
-  content: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+  box: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   author: {
-    marginTop: 50,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: 'white',
+    borderWidth: 1,
+    width: '100%',
+    height: 50,
+    marginBottom: 20,
+    borderColor: "#CFCFD0"
   },
   darkFont: {
-    color: "white",
+    color: "white"
   },
   font: {
-    fontSize: 18,
+    fontSize: 18
   },
   item: {
-    width: "85%",
-    height: 40,
-    borderBottomWidth: 1,
+    width: "100%",
+    height: 45,
     borderColor: "#CFCFD0",
     justifyContent: "flex-start",
     alignItems: "center",
     flexDirection: "row",
+    backgroundColor: 'white',
+    // borderWidth: 1,
+    // borderColor: 'red'
   },
   itemLeft: {
-    width: "12%",
+    width: "20%",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   itemMid: {
-    width: "75%",
+    width: "65%",
   },
   itemRight: {
-    width: "25%",
-  },
+    width: "20%",
+  }
 });
 
 export default MoreScreen;
--- screens/more/MoreScreenStack.js
+++ screens/more/MoreScreenStack.js
@@ -2,15 +2,16 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, StatusBar } from "react-native";
 
-import SystemMapNavigator from '../system-map/SystemMapNavigator';
-import MoreScreen from '../more/MoreScreen';
+import SystemMapNavigator from "../system-map/SystemMapNavigator";
+import MoreScreen from "../more/MoreScreen";
+import FeedbackScreen from "../more/FeedbackScreen";
 
 const Stack = createStackNavigator();
 
 function MoreScreenStack() {
   return (
-    <Stack.Navigator initialRouteName="More Screen">
-      <Stack.Screen name="More Screen" component={MoreScreen} />
+    <Stack.Navigator initialRouteName="Bart X">
+      <Stack.Screen name="Bart X" component={MoreScreen} />
       <Stack.Screen name="System Map" component={SystemMapNavigator} />
     </Stack.Navigator>
   );
