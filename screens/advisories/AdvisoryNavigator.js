901599e3f14929cdc2fc0a633e439eddb4e63026
yonureker <yonureker@gmail.com>
2020-06-24 23:55:02 -0700

    remove colorScheme

--- screens/advisories/AdvisoryNavigator.js
+++ screens/advisories/AdvisoryNavigator.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useColorScheme } from "react-native-appearance";
-
 import AdvisoryScreen from "../advisories/AdvisoryScreen";
 
 const Stack = createStackNavigator();
 
 export default function AdvisoryNavigator() {
-  const scheme = useColorScheme();
-
   return (
     <Stack.Navigator
       initialRouteName="Advisories"
@@ -19,7 +15,11 @@ export default function AdvisoryNavigator() {
         }
       }}
     >
-      <Stack.Screen name="Advisories" component={AdvisoryScreen} />
+      <Stack.Screen
+        name="Advisories"
+        component={AdvisoryScreen}
+        options={() => ({ headerTitleAlign: "center" })}
+      />
     </Stack.Navigator>
   );
 }
