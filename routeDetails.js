7d736fc56b76c81afbc16c8a22eee6215342f5c0
yonureker <yonureker@gmail.com>
2020-06-28 23:49:32 -0700

    create file

---
+++ routeDetails.js
@@ -0,0 +1,489 @@
+module.exports.routes = {
+  route: [
+    ,
+    ,
+    {
+      hexcolor: "#339933",
+      coordinates: [
+        {
+          latitude: 37.706121,
+          longitude: -122.469081
+        },
+        {
+          latitude: 37.721585,
+          longitude: -122.447506
+        },
+        {
+          latitude: 37.733064,
+          longitude: -122.433817
+        },
+        {
+          latitude: 37.75247,
+          longitude: -122.418143
+        },
+        {
+          latitude: 37.765062,
+          longitude: -122.419694
+        },
+        {
+          latitude: 37.779732,
+          longitude: -122.414123
+        },
+        {
+          latitude: 37.784471,
+          longitude: -122.407974
+        },
+        {
+          latitude: 37.789405,
+          longitude: -122.401066
+        },
+        {
+          latitude: 37.792874,
+          longitude: -122.39702
+        },
+        {
+          latitude: 37.804872,
+          longitude: -122.29514
+        },
+        {
+          latitude: 37.797027,
+          longitude: -122.26518
+        },
+        {
+          latitude: 37.774836,
+          longitude: -122.224175
+        },
+        {
+          latitude: 37.753661,
+          longitude: -122.196869
+        },
+        {
+          latitude: 37.721947,
+          longitude: -122.160844
+        },
+        {
+          latitude: 37.696924,
+          longitude: -122.126514
+        },
+        {
+          latitude: 37.669723,
+          longitude: -122.087018
+        },
+        {
+          latitude: 37.634375,
+          longitude: -122.057189
+        },
+        {
+          latitude: 37.59063,
+          longitude: -122.017388
+        },
+        {
+          latitude: 37.557465,
+          longitude: -121.976608
+        },
+        {
+          latitude: 37.502171,
+          longitude: -121.939313
+        },
+        {
+          latitude: 37.410277,
+          longitude: -121.891081
+        },
+        {
+          latitude: 37.368473,
+          longitude: -121.874681
+        }
+      ]
+    },
+    ,
+    {
+      hexcolor: "#FF9933",
+      coordinates: [
+        {
+          latitude: 37.368473,
+          longitude: -121.874681
+        },
+        {
+          latitude: 37.410277,
+          longitude: -121.891081
+        },
+        {
+          latitude: 37.502171,
+          longitude: -121.939313
+        },
+        {
+          latitude: 37.557465,
+          longitude: -121.976608
+        },
+        {
+          latitude: 37.59063,
+          longitude: -122.017388
+        },
+        {
+          latitude: 37.634375,
+          longitude: -122.057189
+        },
+        {
+          latitude: 37.669723,
+          longitude: -122.087018
+        },
+        {
+          latitude: 37.696924,
+          longitude: -122.126514
+        },
+        {
+          latitude: 37.721947,
+          longitude: -122.160844
+        },
+        {
+          latitude: 37.753661,
+          longitude: -122.196869
+        },
+        {
+          latitude: 37.774836,
+          longitude: -122.224175
+        },
+        {
+          latitude: 37.797027,
+          longitude: -122.26518
+        },
+        {
+          latitude: 37.803768,
+          longitude: -122.27145
+        },
+        {
+          latitude: 37.80835,
+          longitude: -122.268602
+        },
+        {
+          latitude: 37.829065,
+          longitude: -122.26704
+        },
+        {
+          latitude: 37.852803,
+          longitude: -122.270062
+        },
+        {
+          latitude: 37.870104,
+          longitude: -122.268133
+        },
+        {
+          latitude: 37.873967,
+          longitude: -122.28344
+        },
+        {
+          latitude: 37.902632,
+          longitude: -122.298904
+        },
+        {
+          latitude: 37.925086,
+          longitude: -122.316794
+        },
+        {
+          latitude: 37.936853,
+          longitude: -122.353099
+        }
+      ]
+    },
+    ,
+    {
+      hexcolor: "#FF0000",
+      coordinates: [
+        {
+          latitude: 37.706121,
+          longitude: -122.469081
+        },
+        {
+          latitude: 37.721585,
+          longitude: -122.447506
+        },
+        {
+          latitude: 37.733064,
+          longitude: -122.433817
+        },
+        {
+          latitude: 37.75247,
+          longitude: -122.418143
+        },
+        {
+          latitude: 37.765062,
+          longitude: -122.419694
+        },
+        {
+          latitude: 37.779732,
+          longitude: -122.414123
+        },
+        {
+          latitude: 37.784471,
+          longitude: -122.407974
+        },
+        {
+          latitude: 37.789405,
+          longitude: -122.401066
+        },
+        {
+          latitude: 37.792874,
+          longitude: -122.39702
+        },
+        {
+          latitude: 37.804872,
+          longitude: -122.29514
+        },
+        {
+          latitude: 37.803768,
+          longitude: -122.27145
+        },
+        {
+          latitude: 37.80835,
+          longitude: -122.268602
+        },
+        {
+          latitude: 37.829065,
+          longitude: -122.26704
+        },
+        {
+          latitude: 37.852803,
+          longitude: -122.270062
+        },
+        {
+          latitude: 37.870104,
+          longitude: -122.268133
+        },
+        {
+          latitude: 37.873967,
+          longitude: -122.28344
+        },
+        {
+          latitude: 37.902632,
+          longitude: -122.298904
+        },
+        {
+          latitude: 37.925086,
+          longitude: -122.316794
+        },
+        {
+          latitude: 37.936853,
+          longitude: -122.353099
+        }
+      ]
+    },
+    ,
+    {
+      hexcolor: "#D5CFA3",
+      coordinates: [
+        {
+          latitude: 37.713238,
+          longitude: -122.212191
+        },
+        {
+          latitude: 37.753661,
+          longitude: -122.196869
+        }
+      ]
+    },
+    {
+      hexcolor: "#FFFF33",
+      coordinates: [
+        {
+          latitude: 37.615966,
+          longitude: -122.392409
+        },
+        {
+          latitude: 37.637761,
+          longitude: -122.416287
+        },
+        {
+          latitude: 37.664245,
+          longitude: -122.44396
+        },
+        {
+          latitude: 37.684638,
+          longitude: -122.466233
+        },
+        {
+          latitude: 37.706121,
+          longitude: -122.469081
+        },
+        {
+          latitude: 37.721585,
+          longitude: -122.447506
+        },
+        {
+          latitude: 37.733064,
+          longitude: -122.433817
+        },
+        {
+          latitude: 37.75247,
+          longitude: -122.418143
+        },
+        {
+          latitude: 37.765062,
+          longitude: -122.419694
+        },
+        {
+          latitude: 37.779732,
+          longitude: -122.414123
+        },
+        {
+          latitude: 37.784471,
+          longitude: -122.407974
+        },
+        {
+          latitude: 37.789405,
+          longitude: -122.401066
+        },
+        {
+          latitude: 37.792874,
+          longitude: -122.39702
+        },
+        {
+          latitude: 37.804872,
+          longitude: -122.29514
+        },
+        {
+          latitude: 37.803768,
+          longitude: -122.27145
+        },
+        {
+          latitude: 37.80835,
+          longitude: -122.268602
+        },
+        {
+          latitude: 37.829065,
+          longitude: -122.26704
+        },
+        {
+          latitude: 37.844702,
+          longitude: -122.251371
+        },
+        {
+          latitude: 37.878361,
+          longitude: -122.183791
+        },
+        {
+          latitude: 37.893176,
+          longitude: -122.12463
+        },
+        {
+          latitude: 37.905522,
+          longitude: -122.067527
+        },
+        {
+          latitude: 37.928468,
+          longitude: -122.056012
+        },
+        {
+          latitude: 37.973737,
+          longitude: -122.029095
+        },
+        {
+          latitude: 38.003193,
+          longitude: -122.024653
+        },
+        {
+          latitude: 38.018914,
+          longitude: -121.945154
+        },
+        {
+          latitude: 38.016941,
+          longitude: -121.889457
+        },
+        {
+          latitude: 37.995388,
+          longitude: -121.78042
+        }
+      ]
+    },
+    ,
+    {
+      hexcolor: "#0099CC",
+      coordinates: [
+        {
+          latitude: 37.706121,
+          longitude: -122.469081
+        },
+        {
+          latitude: 37.721585,
+          longitude: -122.447506
+        },
+        {
+          latitude: 37.733064,
+          longitude: -122.433817
+        },
+        {
+          latitude: 37.75247,
+          longitude: -122.418143
+        },
+        {
+          latitude: 37.765062,
+          longitude: -122.419694
+        },
+        {
+          latitude: 37.779732,
+          longitude: -122.414123
+        },
+        {
+          latitude: 37.784471,
+          longitude: -122.407974
+        },
+        {
+          latitude: 37.789405,
+          longitude: -122.401066
+        },
+        {
+          latitude: 37.792874,
+          longitude: -122.39702
+        },
+        {
+          latitude: 37.804872,
+          longitude: -122.29514
+        },
+        {
+          latitude: 37.797027,
+          longitude: -122.26518
+        },
+        {
+          latitude: 37.774836,
+          longitude: -122.224175
+        },
+        {
+          latitude: 37.753661,
+          longitude: -122.196869
+        },
+        {
+          latitude: 37.721947,
+          longitude: -122.160844
+        },
+        {
+          latitude: 37.696924,
+          longitude: -122.126514
+        },
+        {
+          latitude: 37.690746,
+          longitude: -122.075602
+        },
+        {
+          latitude: 37.699756,
+          longitude: -121.92824
+        },
+        {
+          latitude: 37.701687,
+          longitude: -121.899179
+        }
+      ]
+    },
+    {
+      hexcolor: "#C463C5",
+      coordinates: [
+        {
+          latitude: 37.600271,
+          longitude: -122.386702
+        },
+        {
+          latitude: 37.615966,
+          longitude: -122.392409
+        }
+      ]
+    }
+  ]
+};
