e911e126ccca1ed31abc124b931f3de8dcb9d5a3
yonureker <yonureker@gmail.com>
2020-07-19 23:42:56 -0700

    create screen

--- screens/rewards/MyAccountScreen.js
+++ screens/rewards/MyAccountScreen.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import firebase from "../../config/firebaseConfig";
+import "@firebase/auth";
+import "firebase/firestore";
+
+let db = firebase.firestore();
 
 export default function MyAccountScreen(props) {
+  const [points, setPoints] = useState(0);
+  const user = firebase.auth().currentUser;
+
+  useEffect(() => {
+    getAccountBalance()
+  }, [])
+
+  const getAccountBalance = async () => {
+    const userRef = await db.collection('users').doc(user.uid).get()
+    const userPoints = await userRef.data().pointsBalance
+    setPoints(userPoints)
+  }
+
   return (
     <View style={styles.container}>
+      <View
+          style={{
+            width: "100%",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.27,
+            shadowRadius: 4.65,
+
+            elevation: 6,
+          }}
+        ><Text>You have {points} points</Text></View>
       <TouchableOpacity
         style={styles.sendCode}
         onPress={() => firebase.auth().signOut().then(props.navigation.navigate('Rewards Home'))}
       >
         <Text>Log Out</Text>
+        <Text>{user.uid}</Text>
+        
       </TouchableOpacity>
     </View>
   );
