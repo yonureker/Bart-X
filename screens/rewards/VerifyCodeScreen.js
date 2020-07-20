43d00459a38dfcda3ebe5a2a5cb48106eef413b0
yonureker <yonureker@gmail.com>
2020-07-19 23:43:06 -0700

    fix atob issue

--- screens/rewards/VerifyCodeScreen.js
+++ screens/rewards/VerifyCodeScreen.js
@@ -10,14 +10,24 @@ import {
   Keyboard,
   Alert,
 } from "react-native";
-import {MaterialCommunityIcons} from '@expo/vector-icons';
-import * as firebase from "firebase";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import firebase from "../../config/firebaseConfig";
 import "@firebase/auth";
+import "firebase/firestore";
 
+// this is needed because of an issue with Firebase JS SDK with React Native
+import { decode, encode } from "base-64";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
+
+// initialize database
+let db = firebase.firestore();
 
 export default function VerifyCodeScreen(props) {
-
-
   const [code, setCode] = useState("");
 
   const confirmCode = () => {
@@ -25,15 +35,27 @@ export default function VerifyCodeScreen(props) {
       props.route.params.verificationId,
       code
     );
+
     firebase
       .auth()
       .signInWithCredential(credential)
       .then((result) => {
-        // Do something with the results here
-        console.log('logged in');
+        if (result.user) {
+          props.navigation.navigate("My Account");
+        }
+
+        if (result.additionalUserInfo.isNewUser) {
+          db.collection("users")
+            .doc(firebase.auth().currentUser.uid)
+            .set({
+              pointsBalance: 10,
+              clippercardNumber: null,
+              phoneNumber: firebase.auth().currentUser.phoneNumber,
+            });
+        }
       })
-      .catch(error => console.log(error));
-  }
+      .catch((error) => console.log(error));
+  };
 
   return (
     <KeyboardAvoidingView
@@ -41,111 +63,87 @@ export default function VerifyCodeScreen(props) {
       style={{ flex: 1 }}
     >
       <View style={styles.container}>
+        <View
+          style={{
+            width: "100%",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.27,
+            shadowRadius: 4.65,
+
+            elevation: 6,
+          }}
+        >
+          <Image
+            style={{ height: "100%", width: "100%", borderRadius: 20 }}
+            source={require("../../assets/verify.jpg")}
+          />
+        </View>
+        <View
+          style={{
+            width: "100%",
+            flex: 1,
+            justifyContent: "space-evenly",
+            marginTop: 10,
+          }}
+        >
+          <View style={{ alignSelf: "flex-start" }}>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+              Verify your phone number
+            </Text>
+          </View>
+          <View>
+            <Text>Enter the 6-digit code you received via SMS.</Text>
+          </View>
           <View
             style={{
-              width: "100%",
-              flex: 1,
+              flexDirection: "row",
               justifyContent: "center",
               alignItems: "center",
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 3,
-              },
-              shadowOpacity: 0.27,
-              shadowRadius: 4.65,
-
-              elevation: 6,
             }}
           >
-            <Image
-              style={{ height: "100%", width: "100%", borderRadius: 20 }}
-              source={require("../../assets/verify.jpg")}
-            />
+            <View>
+              <MaterialCommunityIcons
+                name="cellphone-message"
+                size={20}
+                color="black"
+                style={{ paddingLeft: 10, paddingRight: 10 }}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <TextInput
+                placeholder="Enter Confirmation Code"
+                onChangeText={(code) => setCode(code)}
+                keyboardType="phone-pad"
+                autoCompleteType="tel"
+                returnKeyType="done"
+                style={{ borderBottomWidth: 1, borderBottomColor: "#DDDDDD" }}
+              ></TextInput>
+            </View>
           </View>
           <View
             style={{
+              backgroundColor: "#0A99CB",
               width: "100%",
-              flex: 1,
-              justifyContent: "space-evenly",
-              marginTop: 10,
+              height: 40,
+              borderRadius: 10,
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <View style={{ alignSelf: "flex-start" }}>
-              <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-                Verify your phone number
-              </Text>
-            </View>
-            <View>
-              <Text>
-                Enter the 6-digit code you received via SMS.
-              </Text>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <View>
-              <MaterialCommunityIcons
-          name="cellphone-message"
-          size={20}
-          color="black"
-          style={{ paddingLeft: 10, paddingRight: 10 }}
-        />
-              </View>
-              <View style={{ flex: 1 }}>
-                <TextInput
-                  placeholder="Enter Confirmation Code"
-                  onChangeText={(code) => setCode(code)}
-                  keyboardType="phone-pad"
-                  autoCompleteType="tel"
-                  returnKeyType="done"
-                  style={{borderBottomWidth: 1, borderBottomColor: '#DDDDDD'}}
-                ></TextInput>
-              </View>
-            </View>
-            <View
-              style={{
-                backgroundColor: "#0A99CB",
-                width: "100%",
-                height: 40,
-                borderRadius: 10,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <TouchableOpacity onPress={confirmCode}>
-                <Text style={{ color: "white" }}>Confirm Code</Text>
-              </TouchableOpacity>
-            </View>
-            {/* <View><TextInput
-                  placeholder="Verfication Code"
-                  onChangeText={(text) => setCode(text)}
-                  keyboardType="phone-pad"
-                  autoCompleteType="tel"
-                  returnKeyType="done"
-                  maxLength={10}
-                ></TextInput></View>
-            <View
-              style={{
-                backgroundColor: "#0A99CB",
-                width: "100%",
-                height: 40,
-                borderRadius: 10,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <TouchableOpacity onPress={confirmCode}>
-                <Text style={{ color: "white" }}>Confirm Code</Text>
-              </TouchableOpacity>
-            </View> */}
-            <View></View>
-            <View></View>
+            <TouchableOpacity onPress={confirmCode}>
+              <Text style={{ color: "white" }}>Confirm Code</Text>
+            </TouchableOpacity>
           </View>
+          <View></View>
+          <View></View>
+        </View>
       </View>
     </KeyboardAvoidingView>
   );
